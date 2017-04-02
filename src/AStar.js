a30b53503582d2261ff806f610a781ff0560a65e
firepick1 (localhost) <karl@firepick.org>
2017-04-02 11:14:55 -0700

    astar

--- src/AStar.js
+++ src/AStar.js
@@ -1,68 +1,67 @@
 var mathjs = require("mathjs");
 
-(function(exports) { class AStar {
-    constructor() {
-        this.queue = [];
-    }
-
-} //// CLASS
-
-    module.exports = exports.AStar = AStar;
-})(typeof exports === "object" ? exports : (exports = {}));
-
-(typeof describe === 'function') && describe("AStar", function() {
-    var should = require("should");
-
-    it("graph", function() {
-        class Node {
-            constructor(name, neighborsOf) {
-                this.name = name;
-                this.neighborsOf = neighborsOf;
-                this.from = [];
-                this.fScore = Number.MAX_SAFE_INTEGER;
-                this.gScore = Number.MAX_SAFE_INTEGER;
-            }
-            neighbors() {
-                return this.$neighbors || (this.$neighbors = neighborsOf(this));
-            }
-            pathTo(goal) {
-                var openSet = [this];
-                this.fScore = hce(this,goal);
-                this.gScore = 0;
-                while (openSet.length) {
-                    openSet.sort((a,b) => a.fScore - b.fScore);
-                    console.log("openSet", openSet.map((node) => node.name));
-                    var current = openSet.shift();
-                    current.isOpen = false;
-                    //console.log("current", current.name, openSet.map((node) => node.name));
-                    if (current.name === "END") {
-                        totalPath = [current];
-                        while (current.cameFrom) {
-                            current = current.cameFrom;
-                            totalPath.push(current);
-                        }
-                        return totalPath;
+(function(exports) { 
+    class AStarNode {
+        constructor(name, neighborsOf) {
+            this.name = name;
+            this.neighborsOf = neighborsOf;
+            this.from = [];
+        }
+        neighbors() {
+            return this.$neighbors || (this.$neighbors = this.neighborsOf(this));
+        }
+        score(fScoreMap) {
+            var score = fScoreMap[this.name];
+            return score == null ? Number.MAX_SAFE_INTEGER : score;
+        }
+        pathTo(goal, estimateCost) {
+            var openSet = [this];
+            var fScoreMap = {};
+            var gScoreMap = {};
+            fScoreMap[this.name] = estimateCost(this, goal);
+            gScoreMap[this.name] = 0;
+            while (openSet.length) {
+                openSet.sort((a,b) => a.score(fScoreMap) - b.score(fScoreMap));
+                console.log("openSet", openSet.map((node) => node.name));
+                var current = openSet.shift();
+                current.isOpen = false;
+                if (current === goal) {
+                    totalPath = [current];
+                    while (current.cameFrom) {
+                        current = current.cameFrom;
+                        totalPath.push(current);
                     }
-                    current.isClosed = true;
-                    current.neighbors().forEach((neighbor) => {
-                        if (!neighbor.isClosed) {
-                            var tentative_gScore = current.gScore + hce(current, neighbor);
-                            if (!neighbor.isOpen) {
-                                neighbor.isOpen = true;
-                                openSet.push(neighbor);
-                            } else if (tentative_gScore >= neighbor.gScore) {
-                                neighbor = null;
-                            }
-                            if (neighbor) {
-                                neighbor.cameFrom = current;
-                                neighbor.gScore = tentative_gScore;
-                                neighbor.fScore = hce(neighbor, goal);
-                            }
-                        }
-                    });
+                    return totalPath;
                 }
+                current.isClosed = true;
+                current.neighbors().forEach((neighbor) => {
+                    if (!neighbor.isClosed) {
+                        var tentative_gScore = current.score(gScoreMap) + estimateCost(current, neighbor);
+                        if (!neighbor.isOpen) {
+                            neighbor.isOpen = true;
+                            openSet.push(neighbor);
+                        } else if (tentative_gScore >= neighbor.score(gScoreMap)) {
+                            neighbor = null;
+                        }
+                        if (neighbor) {
+                            neighbor.cameFrom = current;
+                            gScoreMap[neighbor.name] = tentative_gScore;
+                            fScoreMap[neighbor.name] = estimateCost(neighbor, goal);
+                        }
+                    }
+                });
             }
         }
+    }
+
+    module.exports = exports.AStarNode = AStarNode;
+})(typeof exports === "object" ? exports : (exports = {}));
+
+(typeof describe === 'function') && describe("AStarNode", function() {
+    var should = require("should");
+    var AStarNode = exports.AStarNode;
+
+    it("graph", function() {
         var costs = {
             START: {
                 A: 1, // huge hills
@@ -84,17 +83,17 @@ var mathjs = require("mathjs");
             Object.keys(nodeCosts).forEach((name) => {
                 var neighbor = graph[name];
                 if (!neighbor) {
-                    neighbor = graph[name] = new Node(name, neighborsOf);
+                    neighbor = graph[name] = new AStarNode(name, neighborsOf);
                 }
                 neighbors.push(neighbor);
             });
             return neighbors;
         }
 
-        graph.START = new Node("START", neighborsOf);
-        graph.END = new Node("END");
+        graph.START = new AStarNode("START", neighborsOf);
+        graph.END = new AStarNode("END", neighborsOf);
 
-        var hce = (n1, n2, goal) => {
+        var costHeuristic = (n1, n2) => {
             if (n1 === n2) {
                 return 0;
             }
@@ -113,11 +112,11 @@ var mathjs = require("mathjs");
             }
             return cost;
         }
-        hce(graph.START,graph.END).should.equal(1);
-        hce(graph.B,graph.B1).should.equal(3);
-        hce(graph.B1,graph.END).should.equal(3);
+        costHeuristic(graph.START,graph.END).should.equal(1);
+        costHeuristic(graph.B,graph.B1).should.equal(3);
+        costHeuristic(graph.B1,graph.END).should.equal(3);
 
-        var path = graph.START.pathTo(graph.END);
+        var path = graph.START.pathTo(graph.END, costHeuristic);
         console.log("path", path.map((node) => node.name));
     })
 })
