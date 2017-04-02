856a6c82966e8860131f72b07d14ffb83e0cb0cf
firepick1 (localhost) <karl@firepick.org>
2017-04-02 11:59:06 -0700

    astar

--- src/AStar.js
+++ src/AStar.js
@@ -4,7 +4,6 @@ var mathjs = require("mathjs");
     class AStarNode {
         constructor(name) {
             this.name = name;
-            this.from = [];
         }
         score(nameScoreMap) {
             var score = nameScoreMap[this.name];
